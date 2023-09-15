1b8260aa429292f8b88d291dc7ad9afe402a3f64
Dmytro Hubych <dh.hubych@gmail.com>
2023-09-15 19:41:24 +0300

    added motions to benefits section

--- src/components/benefits/Benefits.tsx
+++ src/components/benefits/Benefits.tsx
@@ -27,6 +27,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 }
+  },
+}
+
 type Props = {
   setSelectedPage: Dispatch<SetStateAction<SelectedPage>>;
 }
@@ -42,15 +49,31 @@ const Benefits: FC<Props> = ({ setSelectedPage }) => {
 
       >
         {/* HEADER */}
-        <div className='md:my-5 md:w-3/5'>
+        <motion.div
+          className='md:my-5 md:w-3/5'
+          initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -200 },
+              visible: { opacity: 1, x: 0 },
+            }}
+        >
           <HText text="MORE THAN JUST GYM." />
           <p className='my-5 text-sm'>
             We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.
           </p>
-        </div>
+        </motion.div>
 
         {/* BENEFITS */}
-        <div className='md:flex items-center justify-between gap-8 mt-5'>
+        <motion.div
+          className='md:flex items-center justify-between gap-8 mt-5'
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
           {benefits.map(benefit => (
             <EachBenefit
               key={benefit.title}
@@ -60,7 +83,7 @@ const Benefits: FC<Props> = ({ setSelectedPage }) => {
               setSelectedPage={setSelectedPage}
             />
           ))} 
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   )
--- src/components/benefits/EachBenefit.tsx
+++ src/components/benefits/EachBenefit.tsx
@@ -1,6 +1,12 @@
 import { SelectedPage } from "@/types";
+import { motion } from "framer-motion";
 import { Dispatch, FC, SetStateAction } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+}
 
 type Props = {
   icon: JSX.Element,
@@ -16,7 +22,10 @@ const EachBenefit: FC<Props> = ({
   setSelectedPage,
 }) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+      variants={childVariant}
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
@@ -38,7 +47,7 @@ const EachBenefit: FC<Props> = ({
       >
         Learn More
       </AnchorLink>
-    </div>
+    </motion.div>
   )
 }
 
