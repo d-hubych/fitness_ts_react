fbab339c2081c98aa290b79308c3208a9d071b7d
Dmytro Hubych <dh.hubych@gmail.com>
2023-09-15 21:07:32 +0300

    more motions to benefits)

--- src/components/benefits/Benefits.tsx
+++ src/components/benefits/Benefits.tsx
@@ -54,13 +54,13 @@ const Benefits: FC<Props> = ({ setSelectedPage }) => {
         <motion.div
           className='md:my-5 md:w-3/5'
           initial='hidden'
-            whileInView='visible'
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
-            variants={{
-              hidden: { opacity: 0, x: -200 },
-              visible: { opacity: 1, x: 0 },
-            }}
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.3, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -200 },
+            visible: { opacity: 1, x: 0 },
+          }}
         >
           <HText children="MORE THAN JUST GYM." />
           <p className='my-5 text-sm'>
@@ -101,18 +101,38 @@ const Benefits: FC<Props> = ({ setSelectedPage }) => {
             {/* TITLE */}
             <div className='relative'>
               <div className='before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves'>
-                <HText>
-                  {'MILLIONS OF HAPPY MEMBERS GETTING '}
+                <motion.div
+                  initial='hidden'
+                  whileInView='visible'
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: 0.3, duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -200 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    {'MILLIONS OF HAPPY MEMBERS GETTING '}
 
-                  <span className='text-primary-500'>
-                    FIT
-                  </span>
-                </HText>
+                    <span className='text-primary-500'>
+                      FIT
+                    </span>
+                  </HText>
+                </motion.div>
               </div>
             </div>
 
             {/* DESCRIPT */}
-            <div>
+            <motion.div
+              initial='hidden'
+              whileInView='visible'
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 200 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <p className='my-5'>
                 Nascetur aenean massa auctor tincidunt. Iaculis potenti amet egestas ultrices consectetur adipiscing ultricies enim. Pulvinar fames vitae vitae quis. Quis amet vulputate tincidunt at in nulla nec. Consequat sed facilisis dui sit egestas ultrices tellus. Ullamcorper arcu id pretium sapien proin integer nisl. Felis orci diam odio.
               </p>
@@ -120,7 +140,7 @@ const Benefits: FC<Props> = ({ setSelectedPage }) => {
               <p className='mb-5'>
                 Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est tellus quam porttitor. Mauris velit euismod elementum arcu neque facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit enim mattis odio in risus nunc.
               </p>
-            </div>
+            </motion.div>
 
             {/* BUTTON */}
             <div className='relative'>
