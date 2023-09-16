67c43d77ad312835789c72a57189d2f55ea95bc6
Dmytro Hubych <dh.hubych@gmail.com>
2023-09-16 19:16:14 +0300

    contactUS section DONE

--- src/components/contactUs/ContactUs.tsx
+++ src/components/contactUs/ContactUs.tsx
@@ -5,11 +5,27 @@ import { useForm } from 'react-hook-form';
 import ContactUsPageGraphic from '@/assets/ContactUsPageGraphic.png';
 import HText from "../hText/HText";
 
+const inputStyles = 'mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white';
+
 type Props = {
   setSelectedPage: Dispatch<SetStateAction<SelectedPage>>;
 }
 
 const ContactUs: FC<Props> = ({ setSelectedPage }) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+  
+    if (!isValid) {
+      e.preventDefault();
+    }
+  }
+
   return (
     <section
       id="contactus"
@@ -38,9 +54,109 @@ const ContactUs: FC<Props> = ({ setSelectedPage }) => {
           </HText>
 
           <p className="my-5">
-          Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
           </p>
         </motion.div>
+
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 500 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formsubmit.co/b676b71f3ae0d05f3527f493466aa4f9"
+            >
+              <input
+                type="text"
+                placeholder="NAME"
+                className={inputStyles}
+                {...register("name", {
+                  required:  true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && 'This field is required.'}
+                  {errors.name.type === "maxLength" && 'Max length is 100 char.'}
+                </p>
+              )}
+
+              <input
+                type="email"
+                placeholder="EMAIL"
+                
+                className={inputStyles}
+                {...register("email", {
+                  required:  true,
+                  pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && 'This field is required.'}
+                  {errors.name.type === "pattern" && 'Invalid email address.'}
+                </p>
+              )}
+
+              <textarea
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                className={inputStyles}
+                {...register("message", {
+                  required:  true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" && 'This field is required.'}
+                  {errors.message.type === "maxLength" && 'Max length is 2000 char.'}
+                </p>
+              )}
+              
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-100 hover:text-white"
+              >
+                SUBMIT
+              </button>
+            </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div
+              className="md:before:content-evolvetext before:absolute w-full before:-bottom-20 before:-right-10 before:z-[-1]"
+            >
+              <img
+                src={ContactUsPageGraphic}
+                alt="Contac-Us-Page-Graphic"
+                className="w-full"
+              />
+            </div>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   )
